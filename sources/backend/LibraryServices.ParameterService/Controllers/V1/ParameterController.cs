e082a9e09e38851985a414fec6716effe586e688
Young <iamlovedit@163.com>
2023-10-31 10:37:46 +0800

    add parameter service project

---
+++ sources/backend/LibraryServices.ParameterService/Controllers/V1/ParameterController.cs
@@ -0,0 +1,16 @@
+﻿using Asp.Versioning;
+using LibraryServices.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryServices.ParameterService.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("parameter/v{version:apiVersion}")]
+    public class ParameterController : ApiControllerBase
+    {
+        public ParameterController()
+        {
+
+        }
+    }
+}
