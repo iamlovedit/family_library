51c727d355f0d68e39f5dfa16fdd7b19e5f86cb1
Young <iamlovedit@163.com>
2023-11-10 18:03:42 +0800

    reorganzie files

--- sources/frontend/gala_web/src/components/Layout/SiteHeader/Index.vue
+++ sources/frontend/gala_web/src/components/Layout/SiteHeader/Index.vue
@@ -33,7 +33,7 @@
 
 <script setup lang="ts">
 import type { MenuOption } from 'naive-ui';
-import { h, Component, ref, watch, computed } from 'vue'
+import { h, Component, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { NIcon } from 'naive-ui'
 import SiteIcon from '@components/icons/SiteIcon.vue'
@@ -45,31 +45,32 @@ import {
 } from '@vicons/ionicons5'
 import { DarkModeRound, LightModeRound } from '@vicons/material';
 import { useAppStore } from '@/stores/modules/app';
+import { setLocale, t } from '@/locales'
+
 
 
 function renderIcon(icon: Component) {
     return () => h(NIcon, null, { default: () => h(icon) })
 }
-const activeKey = ref<string | null>();
 const route = useRoute();
 const router = useRouter();
 const appStore = useAppStore();
 
 const menuOptions: MenuOption[] = [
     {
-        label: '首页',
+        label: t('header.home'),
         key: 'home',
-        icon: renderIcon(HomeIcon)
+        // icon: renderIcon(HomeIcon)
     },
     {
-        label: '节点包',
+        label: t('header.packages'),
         key: 'packages',
-        icon: renderIcon(BookIcon),
+        // icon: renderIcon(BookIcon),
     },
     {
-        label: '族库',
+        label: t('header.family'),
         key: 'families',
-        icon: renderIcon(FileIcon)
+        // icon: renderIcon(FileIcon)
     }
 ]
 
@@ -81,6 +82,7 @@ const languageOptions = [
         props: {
             onClick: () => {
                 appStore.updateLanguage('en-US')
+                setLocale('en-US') //TODO:does not work
             }
         },
     },
@@ -90,14 +92,15 @@ const languageOptions = [
         props: {
             onClick: () => {
                 appStore.updateLanguage('zh-CN')
+                setLocale('zh-CN') //TODO:does not work
             },
         },
     }
 ];
 
-watch(() => route.path, () => {
-    activeKey.value = route.matched[0].name as string
-})
+const activeKey = computed<string>(() => {
+    return route.name as string;
+});
 
 const themeIcon = computed(() => {
     return appStore.appState.theme == 'dark' ? DarkModeRound : LightModeRound;
@@ -105,10 +108,7 @@ const themeIcon = computed(() => {
 
 function handleUpdateValue(key: string) {
     router.push({
-        name: key,
-        params: {
-            lang: 'zh-CN'
-        }
+        name: key
     })
 }
 
