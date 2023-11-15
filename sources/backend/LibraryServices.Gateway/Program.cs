5ace092de8ec3021ade9fbe2b91fb5e6454aa08f
Young <iamlovedit@163.com>
2023-11-15 23:51:26 +0800

    update codes

--- sources/backend/LibraryServices.Gateway/Program.cs
+++ sources/backend/LibraryServices.Gateway/Program.cs
@@ -1,23 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Configuration.AddOcelot(builder.Environment);
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseOcelot().Wait();
 app.Run();
