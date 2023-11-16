825abae59aaa9d7ebb9de84c58cff962455d539d
Young <iamlovedit@163.com>
2023-11-16 18:06:38 +0800

    update ocelot project

--- sources/backend/LibraryServices.Gateway/Program.cs
+++ sources/backend/LibraryServices.Gateway/Program.cs
@@ -1,14 +1,37 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using LibraryServices.Infrastructure.ServicesExtensions;
+using Ocelot.Provider.Polly;
 
+const string _corsName = "cors";
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddOcelot(builder.Environment);
-builder.Services.AddOcelot(builder.Configuration);
+//builder.Configuration.AddOcelot(builder.Environment);
+var services = builder.Services;
+services.AddOcelot(builder.Configuration).AddPolly();
+services.AddSerilogSetup(builder.Configuration);
+services.AddAuthorizationSetup(builder.Configuration);
+services.AddJwtAuthenticationSetup(builder.Configuration);
+
+services.AddCors(option =>
+{
+    option.AddPolicy(_corsName, policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+});
+
+builder.WebHost.ConfigureAppConfiguration((builderContext, builder) =>
+{
+    builder.SetBasePath(builderContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json", false, true)
+        .AddJsonFile($"appsettings.{builderContext.HostingEnvironment.EnvironmentName}.json", true, false)
+        .AddJsonFile($"ocelot.{builderContext.HostingEnvironment.EnvironmentName}.json", false, false)
+        .AddEnvironmentVariables();
+});
+
 var app = builder.Build();
 
-app.UseAuthorization();
+app.UseCors(_corsName);
 
-app.MapControllers();
-
-app.UseOcelot().Wait();
+await app.UseOcelot().ConfigureAwait(true);
 app.Run();
