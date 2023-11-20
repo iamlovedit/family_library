a1ff9bbe8899f984a18a689ecac3597234d51428
Young <iamlovedit@163.com>
2023-11-20 16:27:06 +0800

    enrich log output

--- sources/backend/LibraryServices.Gateway/Program.cs
+++ sources/backend/LibraryServices.Gateway/Program.cs
@@ -4,11 +4,12 @@ using LibraryServices.Infrastructure.ServicesExtensions;
 using Ocelot.Provider.Polly;
 using Ocelot.Provider.Consul;
 using LibraryServices.Infrastructure.Consul;
+using LibraryServices.Infrastructure.Middlewares;
 const string _corsName = "cors";
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 services.AddOcelot(builder.Configuration).AddConsul().AddConfigStoredInConsul().AddPolly();
-services.AddSerilogSetup(builder.Configuration);
+builder.AddSerilogSetup();
 services.AddHealthChecks();
 services.AddConsulSetup(builder.Configuration);
 
@@ -29,8 +30,12 @@ builder.WebHost.ConfigureAppConfiguration((builderContext, builder) =>
         .AddEnvironmentVariables();
 });
 var app = builder.Build();
+
 app.UseHealthChecks("/health");
+
 app.UseCors(_corsName);
+
+app.UseSerilogLogging();
 
 await app.UseOcelot().ConfigureAwait(true);
 app.Run();
