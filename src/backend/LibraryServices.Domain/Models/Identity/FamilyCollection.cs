046e48ddd2c8774c42716ab398c1c5438b1c10a7
Young <iamlovedit@163.com>
2024-01-02 09:53:25 +0800

    update table names

--- src/backend/LibraryServices.Domain/Models/Identity/FamilyCollection.cs
+++ src/backend/LibraryServices.Domain/Models/Identity/FamilyCollection.cs
@@ -2,19 +2,19 @@
 
 namespace LibraryServices.Domain.Models.Identity
 {
-    [SugarTable(TableName = "family_userCollections")]
+    [SugarTable(TableName = "library_userfavs")]
     public class FamilyCollection : IDeletable
     {
-        [SugarColumn(ColumnName = "collection_familyId", IsPrimaryKey = true)]
+        [SugarColumn(ColumnName = "favs_familyId", IsPrimaryKey = true)]
         public long FamilyId { get; set; }
 
-        [SugarColumn(ColumnName = "collection_userId", IsPrimaryKey = true)]
+        [SugarColumn(ColumnName = "favs_userId", IsPrimaryKey = true)]
         public long UserId { get; set; }
 
-        [SugarColumn(ColumnName = "collection_createDate")]
+        [SugarColumn(ColumnName = "favs_createDate")]
         public DateTime CreateDate { get; set; } = DateTime.Now;
 
-        [SugarColumn(ColumnName = "collection_isDeleted")]
+        [SugarColumn(ColumnName = "favs_isDeleted")]
         public bool IsDeleted { get; set; }
     }
 }
